6280f2598d90783022df9e3746646a4da3eae2a1
Muhammad Saleem <46952459+mesnuuu@users.noreply.github.com>
2022-11-27 03:00:59 +0500

    Singleton Pattern

---
+++ src/main/java/com/mesnu/app/singleton/SingA.java
@@ -0,0 +1,22 @@
+package com.mesnu.app.singleton;
+
+public class SingA {
+
+    private static SingA instance;
+
+    private SingA(){
+        super();
+    }
+
+    public static SingA getInstance(){
+        
+        if(null == instance){
+            synchronized (SingA.class){
+                if(null == instance){
+                    instance = new SingA();
+                }
+            }
+        }
+        return instance;
+    }
+}
--- src/test/java/com/mesnu/app/DesignPatternsApplicationTests.java
+++ src/test/java/com/mesnu/app/DesignPatternsApplicationTests.java
@@ -1,13 +1,36 @@
 package com.mesnu.app;
 
+import com.mesnu.app.singleton.SingA;
+import com.mesnu.app.singleton.SingB;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DesignPatternsApplicationTests {
-
+    @Autowired
+    SingB singB1;
+    @Autowired
+    SingB singB2;
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testSingletons(){
+
+        SingA singA1 = SingA.getInstance();
+        SingA singA2 = SingA.getInstance();
+
+        // singleton test of SignA class
+        Assertions.assertNotNull(singA1);
+        Assertions.assertSame(singA1, singA2);
+
+        // spring bean singleton test
+        Assertions.assertNotNull(singB1);
+        Assertions.assertSame(singB1, singB2);
     }
 
 }
