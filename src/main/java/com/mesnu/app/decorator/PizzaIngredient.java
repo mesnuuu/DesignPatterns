cc603b2c1034a184274e15824b602d785382c5c4
Muhammad Saleem <46952459+mesnuuu@users.noreply.github.com>
2022-11-27 23:00:03 +0500

    Decorator Pattern

---
+++ src/main/java/com/mesnu/app/decorator/PizzaIngredient.java
@@ -0,0 +1,5 @@
+package com.mesnu.app.decorator;
+
+public abstract class PizzaIngredient extends Pizza {
+    public abstract String getDescription();
+}
---
+++ src/main/java/com/mesnu/app/decorator/ThickCrustPizza.java
@@ -0,0 +1,15 @@
+package com.mesnu.app.decorator;
+
+import java.math.BigDecimal;
+
+public class ThickCrustPizza extends Pizza{
+    public ThickCrustPizza(){
+        super();
+        this.description = "Thick Crust Pizza";
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        return new BigDecimal(15.00);
+    }
+}
